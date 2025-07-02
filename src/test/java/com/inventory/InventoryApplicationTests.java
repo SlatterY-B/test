50d738adced360417a9b480142d966cff93ae138
Slatter <u22214085@utp.edu.pe>
2025-07-02 16:32:50 -0500

    Configure H2

--- src/test/java/com/inventory/InventoryApplicationTests.java
+++ src/test/java/com/inventory/InventoryApplicationTests.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest(properties = "spring.datasource.url=jdbc:h2:mem:testdb")
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class InventoryApplicationTests {
 
 	@Test
